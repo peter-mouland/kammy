3f1e5826cb9c865ef26d9eb47c88a864da30b70f
Peter Mouland <peter.mouland@gmail.com>
2018-04-29 15:41:54 +0100

    tweak position retreival

--- scripts/retrieve-player-positions-1718.js
+++ scripts/retrieve-player-positions-1718.js
@@ -8,22 +8,34 @@ import json from './lib/json';
 const log = debug('footy:retrieve-player-positions');
 
 const createJsonObj = (item) => ({
-  [item.player]: {
-    new: item.new,
-    code: item.code,
+  [`${item.surname}, ${item.firstname}`]: {
+    code: item.skyid,
     pos: item.position,
-    player: item.player,
-    club: item.club,
+    player: `${item.surname}, ${item.firstname}`,
+    value: item.value,
+    club: item.team,
+    apps: 0,
+    subs: 0,
+    gls: 0,
+    asts: 0,
+    mom: 0,
+    cs: 0,
+    tb: 0,
+    sb: 0,
+    con: 0,
+    pensv: 0,
+    ycard: 0,
+    rcard: 0,
   }
 });
 
 // for authorising a new sheet look: https://www.npmjs.com/package/google-spreadsheet
 // probably easiest to make the sheet public
-const spreadsheet = new GoogleSpreadsheet('1x2qD0aS6W-MeARu6QT0YthgLV91-Hmlip5_Gut2nEBI', creds);
-const playerListSheet = spreadsheet.getWorksheet('Players');
+const spreadsheet = new GoogleSpreadsheet('1kj4HkXs2kjK__NZ8z9mmtHsg6C-fTQr7_MlUFdEKouw', creds);
+const playerListSheet = spreadsheet.getWorksheet('Auto pick list');
 
 playerListSheet
   .toJson(createJsonObj)
-  .then((jsonData) => json.save(jsonData, './src/assets/ff-1718.next.json'))
+  .then((jsonData) => json.save(jsonData, './ff-1718.json'))
   .then(() => log('done.'))
   .catch((e) => log(e));
